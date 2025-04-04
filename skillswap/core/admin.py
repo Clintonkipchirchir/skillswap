3a14aad13ccd182f2e586e834918776eb4f8cbb7
clinton <clintonkipkemoi2@gmail.com>
2025-04-04 18:10:47 +0300

    Add Profile and Skill models with custom User admin registration

--- skillswap/core/admin.py
+++ skillswap/core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile, Skill
+
+# unregister groups
+admin.site.unregister(Group)
+
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    verbose_name_plural = 'Profile'
+    # This widget makes it easier to manage many-to-many relationships
+    filter_horizontal = ('offered_skills', 'desired_skills',)
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+
+# unregister initial user
+admin.site.unregister(User)
+
+# register new user
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
+admin.site.register(Skill)
--- skillswap/core/models.py
+++ skillswap/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    offered_skills = models.ManyToManyField(Skill, blank=True, related_name='offered')
+    desired_skills = models.ManyToManyField(Skill, blank=True, related_name='desired')
+    
+
+    def __str__(self):
+        return self.user.username
+    
+
+ 
